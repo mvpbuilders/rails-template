ed8bc48fa15c95bafc4435e8f59ef461713fbd69
Felipe Hernandez <hernandezfeli@gmail.com>
2023-04-28 13:35:10 +0200

    feat: Change test

--- files/rails_helper.rb
+++ files/rails_helper.rb
@@ -63,7 +63,14 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::IntegrationHelpers, type: :feature
 end
+
+# require "sidekiq/testing"
+# Sidekiq::Testing.fake!
 
 Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu
